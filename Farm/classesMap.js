804834f82f9e4f0f663259112a884ea106730c15
biiskot <54238803+biiskot@users.noreply.github.com>
2021-03-19 09:56:12 +0100

    test

--- Farm/classesMap.js
+++ Farm/classesMap.js
@@ -2,7 +2,7 @@ class Map {
   constructor(longueur, largeur){
     this.tabMap = new Array(longueur);
 
-    //Création Map 2 dimensions
+    //Création Map tab 2 dimensions
     for (var i = 0; i < this.tabMap.length; i++) {
       this.tabMap[i] = new Array(largeur);
     }
@@ -10,46 +10,57 @@ class Map {
     //Initialiser toutes les cases en herbe :
     for (var i = 0; i < array.length; i++) {
       for (var j = 0; j < array.length; j++) {
-        this.tabMap[i][j] = new BasicParcelle();
+        this.tabMap[i][j] = new Parcelle(); // de base herbe dans le constructor de Parcelle()
       }
     }
 
   }
   placerParcelle(indiceX, indiceY,parcelle){
-    this.tab[indiceX][indiceY] = parcelle;
+    this.tab[indiceX][indiceY].onclick = parcelle;
   }
 }
 
 
 
-class BasicParcelle {
+class Parcelle {
   //Attributs d'une parcelle :
   this.Hauteur = 0;
   this.Largeur = 0;
   this.appearance; // Image a insérer, herbe de base
+  this.plantable = false;
 
   constructor(hauteur,largeur){
     this.Hauteur = hauteur;
     this.Largeur = largeur;
     this.appearance = 'herbe.png';
-    placerParcelle(0,0)
+
   }
 }
 
-class ParceTerre extends BasicParcelle {
+class ParcePlaceMachine extends Parcelle {
 
 }
 
-class ParcePousse extends ParcTerre {
+
+class ParceTerre extends Parcelle {
+  constructor(){
+    super(1,1);
+    this.appearance = 'terre.png';
+    this.plantable = true;
+  }
+}
+
+class ParcePousse extends ParceTerre {
+  constructor(){
+
+  }
+}
+
+class ParcePousseFinie extends ParcePousse {
 
 }
 
-class ParcePousseFinie extends ParcPousse {
-
-}
-
-class ParcePlaceMachine extends BasicParcelle {
-
-}
 
 let Map1 = new Map(20,20);
+let testTerre = new ParceTerre();
+Map.placerParcelle(1,1,testTerre); // Place une parcelle de terre en (1,1)
